039dbbe846d30041cd365c7b413fb2fd5993882f
sergiolopez <sergio.lopez.souto@gmail.com>
2023-05-29 18:48:12 +0200

    controllers decoupled

---
+++ controllers/auth.controller.js
@@ -0,0 +1,41 @@
+const User = require('../models/User.model')
+
+
+// const getAllEvents = (req, res, next) => {
+
+//   Event
+//     .find()
+//     // .sort({date: 1})
+//     .then(response => setTimeout( () => res.json(response), 1500))
+//     .catch(err => next(err))
+    
+// }
+
+// const getOneEvent = (req, res, next) => {
+
+//   const { event_id } = req.params
+
+//   Event
+//     .findById(event_id)
+//     .then(response => res.json(response))
+//     .catch(err => next(err))
+
+// }
+
+
+// const saveEvent = (req, res, next) => {
+
+//   const { name, description, date, imageUrl, location } = req.body
+//   const { _id: creator} = req.payload
+
+//   Event
+//     .create({ name, description, date, imageUrl, location, creator })
+//     .then(response => res.json(response))
+//     .catch(err => next(err))
+
+// }
+
+
+
+
+module.exports = {}
--- routes/event.routes.js
+++ routes/event.routes.js
@@ -2,39 +2,15 @@ const router = require("express").Router()
 
 const { isAuthenticated } = require("../middlewares/verifyToken.middleware")
 
-const Event = require('./../models/Event.model')
+const { getAllEvents, getOneEvent, saveEvent } = require("../controllers/event.controller")
 
 
-router.get("/getAllEvents", (req, res, next) => {
 
-  Event
-    .find()
-    // .sort({date: 1})
-    .then(response => setTimeout( () => res.json(response), 1500))
-    .catch(err => next(err))
-})
+router.get("/getAllEvents" , getAllEvents )
 
+router.get("/getOneEvent/:event_id", getOneEvent)
 
-router.get("/getOneEvent/:event_id", (req, res, next) => {
+router.post("/saveEvent", isAuthenticated, saveEvent)
 
-  const { event_id } = req.params
-
-  Event
-    .findById(event_id)
-    .then(response => res.json(response))
-    .catch(err => next(err))
-})
-
-
-router.post("/saveEvent", isAuthenticated, (req, res, next) => {
-
-  const { name, description, date, imageUrl, location } = req.body
-  const { _id: creator} = req.payload
-
-  Event
-    .create({ name, description, date, imageUrl, location, creator })
-    .then(response => res.json(response))
-    .catch(err => next(err))
-})
 
 module.exports = router
